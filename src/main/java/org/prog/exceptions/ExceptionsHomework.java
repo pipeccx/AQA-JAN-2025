bd118247534b9bbf84d7dd49ecedb65092689709
Viktorija <pipeccx@gmail.com>
2025-01-24 15:54:38 +0200

    Lesson Three

--- src/main/java/org/prog/exceptions/ExceptionsHomework.java
+++ src/main/java/org/prog/exceptions/ExceptionsHomework.java
@@ -1,9 +1,29 @@
 package org.prog.exceptions;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 //TODO: write a method which ca cause NullPointerException
 // TODO: write try-catc-finally to catch NPE
 // TODO: on exception -> print "EXCEPTION CAUGHT!"
 // TODO: always print "DONE TRY-CATCH-FINALLY"
+
 public class ExceptionsHomework {
+    public static void causeNullPointerException() throws NullPointerException {
+        String str = null;
+        System.out.println(str.length());
+    }
+
+    public static void main(String[]args){
+        try {
+            causeNullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("Exception Caught!!");
+        } finally {
+            System.out.println("done try-catch-finally");
+        }
+    }
 }
+
