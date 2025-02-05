e6e6cf1ed89c4bc262e8066760333fb2fe5bae1b
Viktorija <pipeccx@gmail.com>
2025-02-05 18:01:01 +0200

    Lesson 7 homework

--- src/test/java/org/prog/testng/AlloUaNGTest.java
+++ src/test/java/org/prog/testng/AlloUaNGTest.java
@@ -1,7 +1,18 @@
 package org.prog.testng;
 
 
+import org.testng.annotations.AfterClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 //TODO: create separate test-ng.xml file with this class only
 //TODO: move your selenium test to this class
@@ -9,12 +20,20 @@ import org.testng.annotations.Test;
 //TODO: Add assertion that goods name has > 0 length
 
 public class AlloUaNGTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
 
 //    @BeforeClass
 //    public void beforeAllo() {
 //        System.out.println(">>>>>>>>>>ALLO.START");
 //    }
-//
+@BeforeClass
+public void setUp(){
+    System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    driver.manage().window().maximize();
+}
 //    @AfterClass
 //    public void afterAllo() {
 //        System.out.println(">>>>>>>>>>ALLO.END");
@@ -31,27 +50,22 @@ public class AlloUaNGTest {
 //    }
 
     @Test
-    public void ngTest1() {
-        System.out.println("Hello Allo UA Test! 5");
+    public void testSearchForPhone() {
+    driver.get("https://allo.ua/");
+        System.out.println("found");
+        WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(By.name("search")));
+        searchBox.sendKeys("Xiaomi");
+        searchBox.submit();
+        WebElement firstPhone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id=\"__layout\"]/div/div[1]/div[2]/div/div[2]/div[2]/div[1]/div/div[3]/a")));
+String phoneName = firstPhone.getText();
+System.out.println("First phone in search is: " +phoneName);
+        Assert.assertNotNull(phoneName, "Should not be NULL");
+        Assert.assertTrue(phoneName.length() > 0, "Name should be more than one symbol");
     }
-
-    @Test
-    public void ngTest2() {
-        System.out.println("Hello Allo UA Test 1!");
+    @AfterClass
+    public void tearDown(){
+    if (driver != null){
+        driver.quit();
     }
-
-    @Test
-    public void ngTest3() {
-        System.out.println("Hello Allo UA Test 2!");
-    }
-
-    @Test
-    public void ngTest4() {
-        System.out.println("Hello Allo UA Test 3!");
-    }
-
-    @Test
-    public void ngTest5() {
-        System.out.println("Hello Allo UA Test! 4");
     }
 }
